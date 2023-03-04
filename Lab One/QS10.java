4fa83a8408e56b02edd2e763fa18805951ac7ab8
sifatfaysal <sifatfaysall@gmail.com>
2023-03-05 00:15:33 +0600

    Add Java program to find the sum of the first n prime numbers that are odd

---
+++ Lab One/QS10.java
@@ -0,0 +1,33 @@
+package cse101lab1;
+
+import java.util.Scanner;
+
+public class EX10 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n, sum = 1, ctr = 0, num = 0;
+        System.out.print("Enter The Number : ");
+        n = input.nextInt();
+        while (ctr < n) {
+            num++;
+            if (num % 2 != 0) {
+                if (is_Prime(num)) {
+                    sum += num;
+                    ctr++;
+                }
+            }
+        }
+        System.out.println("");
+        System.out.println("Sum of the First " + n + " Prime numbers is: " + sum);
+    }
+
+    public static boolean is_Prime(int num) {
+        for (int i = 3; i * i <= num; i += 2) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
