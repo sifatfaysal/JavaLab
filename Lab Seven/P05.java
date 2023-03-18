40f5ac3f05091723bf0e7dc6d290ee60e2057275
sifatfaysal <sifatfaysall@gmail.com>
2023-03-18 19:47:07 +0600

    Add Java Rectangle and Square classes implementation with main method to get input from the user and calculate their areas and perimeters.

--- Lab Seven/P05.java
+++ Lab Seven/P05.java
@@ -1,5 +1,49 @@
 package javalaab07;
 
+import java.util.Scanner;
 public class P05 {
-    
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter The Rectangle Length : ");
+    int length = input.nextInt();
+    System.out.print("Enter The Rectangle Breadth : ");
+    int breadth = input.nextInt();
+    Rectangle r = new Rectangle(length, breadth);
+    System.out.print("Enter The Square Length : ");
+    int slength = input.nextInt();
+    Square s = new Square(slength);
+    System.out.println();
+    r.printarea();
+    r.printperimeter();
+    s.printarea();
+    s.printperimeter();
+    Square[] a = new Square[10];
+    int k = 5;
+    for (int i = 0; i < 10; i++) {
+      a[i] = new Square(k);
+      k++;
+    }
+    for (int i = 0; i < 10; i++) {
+      a[i].printarea();
+      a[i].printperimeter();
+    }
+  }
+}
+class Rectangle {
+  int length, breadth;
+  Rectangle(int l, int b) {
+    length = l;
+    breadth = b;
+  }
+  void printarea() {
+    System.out.println(length * breadth);
+  }
+  void printperimeter() {
+    System.out.println(2 * (length + breadth));
+  }
+}
+class Square extends Rectangle {
+  Square(int s) {
+    super(s, s);
+  }
 }
