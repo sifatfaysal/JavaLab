0d363d66b86b3b180ef356a43a0b2828ee0dc82a
sifatfaysal <sifatfaysall@gmail.com>
2023-03-31 19:22:47 +0600

    Add Java code for printing diamond pattern

---
+++ LabThree/L32.java
@@ -0,0 +1,38 @@
+package LabThree;
+
+import java.util.Scanner;
+
+public abstract class L32 {
+
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter The Rows : ");
+            int rows = input.nextInt();
+            System.out.println();
+            for (int i = rows / 2; i <= rows; i += 2) {
+                for (int j = 1; j < rows - i; j += 2) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+                for (int j = 1; j <= rows - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            for (int i = rows; i >= 1; i--) {
+                for (int j = i; j < rows; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= (i * 2) - 1; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
