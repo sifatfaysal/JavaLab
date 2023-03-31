2f0c8bfb47f88835ef889601151b7e1f1ac97488
sifatfaysal <sifatfaysall@gmail.com>
2023-03-31 17:40:13 +0600

    Add Java program to print a pyramid pattern of '#' symbols

--- LabThree/L3B.java
+++ LabThree/L3B.java
@@ -22,10 +22,11 @@ public class L3B {
     }
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter Any Number : ");
-        int number = input.nextInt();
-        System.out.println();
-        fibonumber(number);
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter Any Number : ");
+            int number = input.nextInt();
+            System.out.println();
+            fibonumber(number);
+        }
     }
 }
---
+++ LabThree/L3C.java
@@ -0,0 +1,22 @@
+package LabThree;
+
+import java.util.Scanner;
+
+public class L3C {
+    public static void main(String args[]) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter The Rows : ");
+            int row = input.nextInt();
+            System.out.println();
+            for (int i = 0; i < row; i++) {
+                for (int j = row - i; j > 1; j--) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("# ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
