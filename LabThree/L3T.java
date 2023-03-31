333f3323ad215b78f6b16da858479b0dbc586ed7
sifatfaysal <sifatfaysall@gmail.com>
2023-03-31 18:07:13 +0600

    Add Pascal's Triangle implementation

---
+++ LabThree/L3T.java
@@ -0,0 +1,34 @@
+package LabThree;
+
+import java.util.Scanner;
+
+public class L3T {
+    static int fact(int n) {
+        int f;
+        for (f = 1; n > 1; n--) {
+            f *= n;
+        }
+        return f;
+    }
+
+    static int pas(int n, int r) {
+        return fact(n) / (fact(n - r) * fact(r));
+    }
+
+    public static void main(String args[]) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter The Number : ");
+            int num = input.nextInt();
+            System.out.println();
+            for (int i = 0; i <= num; i++) {
+                for (int j = 0; j <= num - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j <= i; j++) {
+                    System.out.print(" " + pas(i, j));
+                }
+                System.out.println();
+            }
+        }
+    }
+}
