06e4f9c6f0354181d7a2eb01ec93c6e7c997330b
sifatfaysal <sifatfaysall@gmail.com>
2023-03-31 17:57:26 +0600

    Add palindrome number checking functionality

---
+++ LabThree/L3M.java
@@ -0,0 +1,24 @@
+package LabThree;
+
+import java.util.Scanner;
+
+public class L3M {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            int sum = 0, temp, r;
+            System.out.print("Enter Any Number : ");
+            int num = input.nextInt();
+            temp = num;
+            while (temp != 0) {
+                r = temp % 10;
+                sum = sum * 10 + r;
+                temp = temp / 10;
+            }
+            if (num == sum) {
+                System.out.println("It is a Palindrome.");
+            } else {
+                System.out.println("Not a Palindrome.");
+            }
+        }
+    }
+}
