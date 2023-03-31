fc12674824ae14c3d63443d9c35fc82cfabbf3de
sifatfaysal <sifatfaysall@gmail.com>
2023-03-31 17:48:19 +0600

    Add code for printing a pattern of numbers in a descending order.

---
+++ LabThree/L3G.java
@@ -0,0 +1,22 @@
+package LabThree;
+
+import java.util.Scanner;
+
+public class L3G {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter The Rows : ");
+            int rows = input.nextInt();
+            System.out.println();
+            for (int i = rows; i >= 1; i--) {
+                for (int j = rows; j > i; j--) {
+                    System.out.print(" ");
+                }
+                for (int k = 1; k <= i; k++) {
+                    System.out.print(k + "");
+                }
+                System.out.println("");
+            }
+        }
+    }
+}
