4f9b365012aebddfda041b0f90a6bbd1711c2af5
sifatfaysal <sifatfaysall@gmail.com>
2023-03-31 19:00:30 +0600

    Add leap year calculation logic

---
+++ LabThree/L3Y.java
@@ -0,0 +1,21 @@
+package LabThree;
+
+import java.util.Scanner;
+
+public class L3Y {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter The Year : ");
+            int year = input.nextInt();
+            if (year % 400 == 0) {
+                System.out.println(year + " is a leap year");
+            } else if (year % 100 == 0) {
+                System.out.println(year + " is not a leap year");
+            } else if (year % 4 == 0) {
+                System.out.println(year + " is a leap year");
+            } else {
+                System.out.println(year + " is not a leap year");
+            }
+        }
+    }
+}
